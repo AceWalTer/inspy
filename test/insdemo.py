d22abb5d59ec0759f748e0cc7f9ca301aa41c22e
yafei.wang <yafei.wang@pisemi.com>
2024-03-29 11:47:28 +0800

    20240329

---
+++ test/insdemo.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+"""
+    @File : insdemo.py \n
+    @Contact : yafei.wang@pisemi.com \n
+    @License: (C)Copyright {} \n
+    @Modify Time: 2023/11/23 10:25 \n
+    @Author : Pisemi Yafei Wang \n
+    @Version: 1.0 \n
+    @Description : None \n
+    @Create Time: 2023/11/23 10:25 \n
+"""
+import pyvisa
+from piinspy import *
+
+if __name__ == '__main__':
+
+    rm = pyvisa.ResourceManager()
+    pi_power_list, pi_dmm_list, pi_smu_list, pi_Load_list = ins_scan(rm)
+    if pi_power_list is not None:
+        for i in pi_power_list:
+            print(i)
+    else:
+        del pi_power_list
+    if pi_dmm_list is not None:
+        for i in pi_dmm_list:
+            print(i)
+    else:
+        del pi_dmm_list
+    if pi_smu_list is not None:
+        for i in pi_smu_list:
+            print(i)
+    else:
+        del pi_smu_list
+    if pi_Load_list is not None:
+        for i in pi_Load_list:
+            print(i)
+    else:
+        del pi_Load_list
